35ff78975250d2e9de7579b18137b635d03c6359
bazsup <supawit.ruen@mail.kmutt.ac.th>
2019-06-01 14:25:36 +0700

    :art: Split tests 🏀

--- hit_me_please/hitters/tests/test_admin.py
+++ hit_me_please/hitters/tests/test_admin.py
@@ -1,18 +1,12 @@
 from django.test import TestCase
 from django.contrib import admin
 
-from .admin import HitterAdmin
-from .models import Hitter
-
-class HitterTest(TestCase):
-    def test_hitter_model_should_have_email_field(self):
-        hitter = Hitter.objects.create(
-            email='supawit@prontomarketing.com'
-        )
-        self.assertEqual(hitter.email, 'supawit@prontomarketing.com')
+from ..admin import HitterAdmin
+from ..models import Hitter
 
 
 class HitterAdminTest(TestCase):
+
     def test_hitter_should_be_registered_to_admin(self):
         self.assertIsInstance(
             admin.site._registry[Hitter],
