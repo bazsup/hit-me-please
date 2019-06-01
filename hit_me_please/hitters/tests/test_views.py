f972c2e4e0be323dad91d414621bf9ef7df1478a
bazsup <supawit.ruen@mail.kmutt.ac.th>
2019-06-01 15:05:09 +0700

    :white_check_mark: Add test save email when submit form 🏀

--- hit_me_please/hitters/tests/test_views.py
+++ hit_me_please/hitters/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 
+from ..models import Hitter
 
 class LandingPageViewTest(TestCase):
     def test_view_should_have_form_with_email_field_and_submit_button(self):
@@ -7,8 +8,25 @@ class LandingPageViewTest(TestCase):
 
         expected = '<form action="." method="post">'
         self.assertContains(response, expected, status_code=200)
+        expected = '<input type="hidden" name="csrfmiddlewaretoken"'
+        self.assertContains(response, expected, status_code=200)
         expected = '<input type="email" name="email">'
         self.assertContains(response, expected, status_code=200)
         expected = '<button type="submit">Submit</button>'
         self.assertContains(response, expected, status_code=200)
+
+    def test_view_should_save_email_when_submit_form(self):
+        email = 'supawit@prontomarketing.com'
+        count = Hitter.objects.filter(email='supawit@prontomarketing.com').count()
+        self.assertEqual(count, 0)
+
+        data = {
+            'email': email
+        }
+        response = self.client.post('/', data=data)
+
+        self.assertEqual(response.status_code, 200)
+
+        count = Hitter.objects.filter(email=email).count()
+        self.assertEqual(count, 1)
 
