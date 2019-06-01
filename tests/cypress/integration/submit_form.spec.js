d74328dc1d7021c679a303fb32200e857909422c
bazsup <supawit.ruen@mail.kmutt.ac.th>
2019-06-01 14:34:54 +0700

    :sparkles: Introduce ATDD render admin hitters 🏀

---
+++ tests/cypress/integration/submit_form.spec.js
@@ -0,0 +1,26 @@
+describe('Landing Page', () => {
+  it('should be able to submit form with email', () => {
+    // const user = cy
+    // user.visit('http://localhost:8000/')
+    // user.get('h1')
+    //     .contains('Hit Me!')
+    // user.get('input[name="email"]')
+    //     .type('supawit@prontomarketing.com')
+    // user.get('button[type="submit"]')
+    //     .click()
+
+    const admin = cy
+    admin.visit('http://localhost:8000/admin/')
+    admin.get('input[name="username"]')
+         .type('bazsup')
+         .get('input[name="password"]')
+         .type('Pronto123')
+         .get('input[type="submit"')
+         .click()
+    admin.get('.model-hitter > th > a')
+         .click()
+    admin.get('.field-email > a')
+         .should('contain', 'supawit@prontomarketing.com')
+
+  })
+})
