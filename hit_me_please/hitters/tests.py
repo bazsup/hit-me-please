e281d6a40444d1f9b43f342d4e1309b88689fa5b
bazsup <supawit.ruen@mail.kmutt.ac.th>
2019-06-01 11:40:29 +0700

    :sparkles: Register Hitter Admin and test! 🏀

--- hit_me_please/hitters/tests.py
+++ hit_me_please/hitters/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
+from django.contrib import admin
 
+from .admin import HitterAdmin
 from .models import Hitter
-
 
 class HitterTest(TestCase):
     def test_hitter_model_should_have_email_field(self):
@@ -9,3 +10,11 @@ class HitterTest(TestCase):
             email='supawit@prontomarketing.com'
         )
         self.assertEqual(hitter.email, 'supawit@prontomarketing.com')
+
+
+class HitterAdminTest(TestCase):
+    def test_hitter_should_be_registered_to_admin(self):
+        self.assertIsInstance(
+            admin.site._registry[Hitter],
+            HitterAdmin
+        )
